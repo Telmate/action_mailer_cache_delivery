a23a447099a0b61cbac91ab89bc0982530637421
Nicolas Ochem <nicolas.ochem@shareurplans.com>
2011-02-28 23:23:15 +0100

    rails 2 compat

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 # Only require this module for the cucumber, selenium and test environments
 if ENV['RAILS_ENV'] == 'cucumber' || ENV['RAILS_ENV'] == 'selenium' || ENV['RAILS_ENV'] == 'test'
-  require File.join(File.dirname(__FILE__), 'lib', 'cache_delivery')
+  require File.join(File.dirname(__FILE__), 'lib', 'action_mailer_cache_delivery')
   if Rails::VERSION::MAJOR >= 3
-    require File.join(File.dirname(__FILE__), 'lib', 'action_mailer_cache_delivery')
+    require File.join(File.dirname(__FILE__), 'lib', 'cache_delivery')
     ActionMailer::Base.add_delivery_method :cache, Mail::CacheDelivery
   end
 end
